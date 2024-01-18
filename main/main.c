54174d7808a2b143822c0ea8f9ab1fe8e96c8bb3
uberwoozle <uberwoozle@gmail.com>
2024-01-18 13:24:32 -0500

    Cleanup code for spiffs initialization

--- main/main.c
+++ main/main.c
@@ -12,27 +12,17 @@
 
 #include "st7565.h"
 #include "bmpfile.h"
-#include "font6x8.h"
-#include "font8x8.h"
-#include "font8x8_bold.h"
+// #include "font6x8.h"
+// #include "font8x8.h"
+// #include "font8x8_bold.h"
 
 #include "gpio_helper.h"
+#include "spiffs_helper.h"
 
 #define	INTERVAL 400
 #define WAIT vTaskDelay(INTERVAL)
 
 static const char *TAG = "ST7565";
-
-static void SPIFFS_Directory(char * path) {
-	DIR* dir = opendir(path);
-	assert(dir != NULL);
-	while (true) {
-		struct dirent*pe = readdir(dir);
-		if (!pe) break;
-		ESP_LOGI(__FUNCTION__,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
-	}
-	closedir(dir);
-}
 
 TickType_t BMPTest(TFT_t * dev, char * file, int width, int height) {
 	TickType_t startTick, endTick, diffTick;
@@ -246,7 +236,6 @@ TFT_t dev;
 static QueueHandle_t gpio_evt_queue = NULL;
 
 static QueueHandle_t task_switch_queue = NULL;
-static SemaphoreHandle_t task_switch_sem;
 
 static SemaphoreHandle_t dickbutt_sem;
 static SemaphoreHandle_t dickbutt_dummy_sem;
@@ -326,39 +315,7 @@ void dickbutt_dummy_task(void *pvParameters)
 
 void app_main(void)
 {
-	ESP_LOGI(TAG, "Initializing SPIFFS");
-
-	esp_vfs_spiffs_conf_t conf = {
-		.base_path = "/spiffs",
-		.partition_label = NULL,
-		.max_files = 16,
-		.format_if_mount_failed =true
-	};
-
-	// Use settings defined above to initialize and mount SPIFFS filesystem.
-	// Note: esp_vfs_spiffs_register is anall-in-one convenience function.
-	esp_err_t ret = esp_vfs_spiffs_register(&conf);
-
-	if (ret != ESP_OK) {
-		if (ret == ESP_FAIL) {
-			ESP_LOGE(TAG, "Failed to mount or format filesystem");
-		} else if (ret == ESP_ERR_NOT_FOUND) {
-			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-		} else {
-			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
-		}
-		return;
-	}
-
-	size_t total = 0, used = 0;
-	ret = esp_spiffs_info(NULL, &total,&used);
-	if (ret != ESP_OK) {
-		ESP_LOGE(TAG,"Failed to get SPIFFS partition information (%s)",esp_err_to_name(ret));
-	} else {
-		ESP_LOGI(TAG,"Partition size: total: %d, used: %d", total, used);
-	}
-
-	SPIFFS_Directory("/spiffs/");
+	spiffs_init();
 
 	// Display init
 	spi_master_init(&dev, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO, CONFIG_CS_GPIO, CONFIG_DC_GPIO, CONFIG_RESET_GPIO, CONFIG_BL_GPIO);
