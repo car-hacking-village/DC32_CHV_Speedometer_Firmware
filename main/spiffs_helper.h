2358aabb4148daf1b8f51f0928973932689a97f9
uberwoozle <uberwoozle@gmail.com>
2024-01-18 13:24:52 -0500

    Created spiffs helper

---
+++ main/spiffs_helper.h
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+
+void spiffs_init();
